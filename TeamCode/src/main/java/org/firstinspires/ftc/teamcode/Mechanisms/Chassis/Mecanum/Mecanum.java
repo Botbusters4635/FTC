c3d2b4d193dffb12b65f2ee5bbbde2805c799c65
Tenzer <neilrodm@gmail.com>
2021-11-19 10:01:15 -0600

    Mecanum Started

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Chassis/Mecanum/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Chassis/Mecanum/Mecanum.java
@@ -4,17 +4,43 @@
 
 package org.firstinspires.ftc.teamcode.Mechanisms.Chassis.Mecanum;
 
+import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
+
 import org.firstinspires.ftc.teamcode.Core.BaseClasses.EctoMechanism;
 
 public class Mecanum extends EctoMechanism {
 
     public Mecanum(String moduleName, String moduleType, MecanumConfig config) {
         super(moduleName, moduleType);
+        mecanumConfig = config;
     }
+
+    MecanumConfig mecanumConfig;
+
+    public Motor frontLeft, backLeft;
+    public Motor frontRight, backRight;
+
+    public MotorGroup allMotors;
+
+    public MecanumDrive mecanum;
 
     @Override
     public void initMechanism() {
+        allMotors.setRunMode(Motor.RunMode.RawPower);
 
+        frontLeft = new MotorEx(hardwareMap, mecanumConfig.getfrontLeftId, mecanumConfig.getGobildaType);
+        backLeft = new MotorEx(hardwareMap, mecanumConfig.getbackLeftId, mecanumConfig.getGobildaType);
+        frontRight = new MotorEx(hardwareMap, mecanumConfig.getfrontRightId, mecanumConfig.getGobildaType);
+        backRight = new MotorEx(hardwareMap, mecanumConfig.getbackRightId, mecanumConfig.getGobildaType);
+
+        allMotors = new MotorGroup(frontLeft, frontRight, backRight, backLeft);
+
+        //motor Setup
+        mecanum = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
     }
 
     @Override
@@ -29,6 +55,7 @@ public class Mecanum extends EctoMechanism {
 
     @Override
     public void stopMechanism() {
-
+        mecanum.stop();
     }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Chassis/Tank/Tank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Chassis/Tank/Tank.java
@@ -71,8 +71,8 @@ public class Tank extends EctoMechanism {
     }
 
     @Override
-    public void stopMechanism() {
-        tank.stop();
+        public void stopMechanism() {
+            tank.stop();
     }
 
 //    @Override
