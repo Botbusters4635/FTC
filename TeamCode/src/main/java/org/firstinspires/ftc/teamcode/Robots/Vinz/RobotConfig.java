f3d3e75ddebc1f60baba7dde62130ad1b336a56c
Tenzer <neilrodm@gmail.com>
2021-12-06 00:10:43 -0600

    master, base system. Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Vinz/RobotConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Vinz/RobotConfig.java
@@ -1,21 +1,5 @@
 package org.firstinspires.ftc.teamcode.Robots.Vinz;
 
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-
-import org.firstinspires.ftc.teamcode.Mechanisms.Arm.ArmConfig;
-import org.firstinspires.ftc.teamcode.Mechanisms.Chassis.Mecanum.MecanumConfig;
-import org.firstinspires.ftc.teamcode.Mechanisms.Intake.IntakeConfig;
-import org.firstinspires.ftc.teamcode.Mechanisms.Spinner.Spinner;
-import org.firstinspires.ftc.teamcode.Mechanisms.Spinner.SpinnerConfig;
-
-public class RobotConfig {
-
-    static public MecanumConfig mecanumConfig = new MecanumConfig("frontLeftMotor", "backLeftMotor", "frontRightMotor", "backRightMotor", Motor.GoBILDA.RPM_223);
-
-    static public ArmConfig armConfig = new ArmConfig("armMotor", 1451.5, 13.5, Motor.GoBILDA.RPM_117);
-
-    static public IntakeConfig intakeConfig = new IntakeConfig("intakeMotor", Motor.GoBILDA.RPM_435);
-
-    static public SpinnerConfig spinnerConfig = new SpinnerConfig("spinnerMotor", Motor.GoBILDA.RPM_117);
-
-}
+//The _RobotConfig_ Class Is Where You Should Create The Configurations for Every Mechanism
+//A Normal Configuration Defines The Name Of The Motors And The _MotorType_
+public class RobotConfig {}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/Vinz/TeleOperatedMode.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Robots.Vinz;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Core.BaseClasses.EctoOpMode;
+
+
+@TeleOp(name = "TeleOp")
+public class TeleOperatedMode extends EctoOpMode {
+
+        /*
+        //Mechanisms
+        */
+
+
+        /*
+        //Controllers
+        */
+
+
+    @Override
+    public void initRobotClasses() {
+
+        /*
+        //Mechanisms
+        */
+
+
+        /*
+        //Controllers
+        */
+
+    }
+
+    //The initRobot() Void Is For Adding The Mechanisms To The MechanismManager Array.
+    @Override
+    public void initRobot() {
+    }
+
+    //The startRobot() Void Is For Moving Certain Mechanisms To Their Start Position
+    //For Example, An Arm Mechanim Would Need To Move To A Certain Position In Order
+    //For It To Start Always In The Same Position
+    @Override
+    public void startRobot() {
+    }
+
+    //The updateRobot() Void Is The Main Loop Of The Code
+    //It Is Being Called Constantly every 10ms
+    //Here You Should Get The Inputs From The Gamepads And Control The Mechanisms With The Inputs
+    @Override
+    public void updateRobot(Double timeStep) {
+    }
+
+}
