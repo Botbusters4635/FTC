a063cfba54a740f2f66f19e0e0b564ba7cab7328
Tenzer <neilrodm@gmail.com>
2022-01-11 14:27:05 -0600

    Paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,32 +10,73 @@ public class MeepMeepTesting {
     MeepMeep meepMeep = new MeepMeep(800);
 
     Pose2d startingPosition = new Pose2d(12, -64, 0);
+    Pose2d wareHouse = new Pose2d(48, -64);
     Pose2d allianceShippingHub = new Pose2d(-12, -42, Math.toRadians(90));
 
-
     RoadRunnerBotEntity bot =
-            new DefaultBotBuilder(meepMeep)
-                    .setConstraints(40, 37, 13.241691158998306, Math.toRadians(214.92695399999997), 10)
-                    .setDimensions(13.5, 15.5)
-                    .followTrajectorySequence(
-                            drive ->
-                                    drive
-                                            .trajectorySequenceBuilder(startingPosition)
-                                            .addDisplacementMarker(
-                                                    () -> {
-                                                      // Subir El Brazo
-                                                    })
-                                            .lineToSplineHeading(allianceShippingHub)
-                                            .addDisplacementMarker(
-                                                    () -> {
-                                                      // Escupir Game Piece
-                                                    })
-                                            .lineToSplineHeading(startingPosition)
-                                            .addDisplacementMarker(
-                                                    () -> {
-                                                      // Bajar Brazo a MEDIUM
-                                                    })
-                                            .build());
+        new DefaultBotBuilder(meepMeep)
+            .setConstraints(40, 37, 13.241691158998306, Math.toRadians(214.92695399999997), 10)
+            .setDimensions(13.5, 15.5)
+            .followTrajectorySequence(
+                drive ->
+                    drive
+                        .trajectorySequenceBuilder(startingPosition)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Subir El Brazo
+                            })
+
+                        // 1st Cycle
+                        .lineToSplineHeading(allianceShippingHub)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Escupir Game Piece
+                            })
+                        .lineToSplineHeading(startingPosition)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Bajar Brazo a MEDIUM
+                            })
+                        .lineToSplineHeading(wareHouse)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Bajar Brazo a MEDIUM
+                            })
+                        .lineToSplineHeading(startingPosition)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Bajar Brazo a MEDIUM
+                            })
+
+                        // 2nd Cycle
+                        .lineToSplineHeading(allianceShippingHub)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Escupir Game Piece
+                            })
+                        .lineToSplineHeading(startingPosition)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Bajar Brazo a MEDIUM
+                            })
+                        .lineToSplineHeading(wareHouse)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Bajar Brazo a MEDIUM
+                            })
+                        .lineToSplineHeading(startingPosition)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Bajar Brazo a MEDIUM
+                            })
+
+                        // 2nd Cycle
+                        .lineToSplineHeading(allianceShippingHub)
+                        .addDisplacementMarker(
+                            () -> {
+                              // Escupir Game Piece
+                            })
+                        .build());
 
     meepMeep
         .setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
